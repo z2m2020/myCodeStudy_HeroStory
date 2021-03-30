020d9805073e68a7016a3ff53a0f0b4f0b6934bb
z2m2020 <bdxx2013@outlook.com>
2021-03-30 16:51:02 +0800

    通过反射完成完成消息类与消息编号映射

--- src/main/java/com/z2m2020/herostory/GameMsgRecognizer.java
+++ src/main/java/com/z2m2020/herostory/GameMsgRecognizer.java
@@ -3,11 +3,18 @@ package com.z2m2020.herostory;
 import com.google.protobuf.GeneratedMessageV3;
 import com.google.protobuf.Message;
 import com.z2m2020.herostory.msg.GameMsgProtocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
 public final class GameMsgRecognizer {
+    /**
+     * 记录日志
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(GameMsgRecognizer.class);
     /**
      * 消息编号->消息对象字典
      */
@@ -15,7 +22,7 @@ public final class GameMsgRecognizer {
     /**
      * 消息类-> 消息编号字典
      */
-    static private final Map<Class<?>,Integer> _clazzAndMsgCodeMap=new HashMap<>();
+    static private final Map<Class<?>,Integer> _msgClazzAndMsgCodeMap=new HashMap<>();
 
 
 
@@ -31,14 +38,71 @@ public final class GameMsgRecognizer {
      * 初始化
      */
     static public void init(){
-        _msgCodeAndMsgObjMap.put(GameMsgProtocol.MsgCode.USER_ENTRY_CMD_VALUE,GameMsgProtocol.UserEntryCmd.getDefaultInstance());
-        _msgCodeAndMsgObjMap.put(GameMsgProtocol.MsgCode.WHO_ELSE_IS_HERE_CMD_VALUE,GameMsgProtocol.WhoElseIsHereCmd.getDefaultInstance());
-        _msgCodeAndMsgObjMap.put(GameMsgProtocol.MsgCode.USER_MOVE_TO_CMD_VALUE,GameMsgProtocol.UserMoveToCmd.getDefaultInstance());
 
-        _clazzAndMsgCodeMap.put(GameMsgProtocol.UserEntryResult.class,GameMsgProtocol.MsgCode.USER_ENTRY_CMD_VALUE);
-        _clazzAndMsgCodeMap.put(GameMsgProtocol.WhoElseIsHereResult.class,GameMsgProtocol.MsgCode.WHO_ELSE_IS_HERE_CMD_VALUE);
-        _clazzAndMsgCodeMap.put(GameMsgProtocol.UserMoveToResult.class,GameMsgProtocol.MsgCode.USER_MOVE_TO_CMD_VALUE);
-        _clazzAndMsgCodeMap.put(GameMsgProtocol.UserQuitResult.class,GameMsgProtocol.MsgCode.USER_QUIT_RESULT_VALUE);
+        LOGGER.info("===完成消息类与消息编号映射===");
+
+        //获取内部类
+        Class<?>[] innerClazzArray=GameMsgProtocol.class.getDeclaredClasses();
+
+        for(Class<?> innerClazz:innerClazzArray){
+            if(null==innerClazz ||
+                    !GeneratedMessageV3.class.isAssignableFrom(innerClazz)){
+                    continue;
+            }
+
+            String clazzName = innerClazz.getSimpleName();
+            clazzName=clazzName.toLowerCase();
+
+            for(GameMsgProtocol.MsgCode msgCode: GameMsgProtocol.MsgCode.values()){
+                if(null==msgCode){
+                    continue;
+                }
+
+                //获取消息编码
+                String strMsgCode=msgCode.name();
+                strMsgCode=strMsgCode.replaceAll("_","");
+                strMsgCode=strMsgCode.toLowerCase();
+
+                if(!strMsgCode.startsWith(clazzName)){
+                    continue;
+                }
+
+                try{
+                    Object returnObj=innerClazz.getDeclaredMethod("getDefaultInstance").invoke(innerClazz);
+                    //打印映射日志
+                    LOGGER.info(
+                            "{}<===>{}",
+                            innerClazz.getName(),
+                            msgCode.getNumber()
+                    );
+
+                    _msgCodeAndMsgObjMap.put(
+                            msgCode.getNumber(),
+                            (GeneratedMessageV3)returnObj
+                    );
+
+                    _msgClazzAndMsgCodeMap.put(
+                            innerClazz,
+                            msgCode.getNumber()
+                    );
+
+                } catch (Exception ex) {
+                    LOGGER.error(ex.getMessage(),ex);
+                }
+            }
+
+        }
+
+
+
+//        _msgCodeAndMsgObjMap.put(GameMsgProtocol.MsgCode.USER_ENTRY_CMD_VALUE,GameMsgProtocol.UserEntryCmd.getDefaultInstance());
+//        _msgCodeAndMsgObjMap.put(GameMsgProtocol.MsgCode.WHO_ELSE_IS_HERE_CMD_VALUE,GameMsgProtocol.WhoElseIsHereCmd.getDefaultInstance());
+//        _msgCodeAndMsgObjMap.put(GameMsgProtocol.MsgCode.USER_MOVE_TO_CMD_VALUE,GameMsgProtocol.UserMoveToCmd.getDefaultInstance());
+//
+//        _clazzAndMsgCodeMap.put(GameMsgProtocol.UserEntryResult.class,GameMsgProtocol.MsgCode.USER_ENTRY_CMD_VALUE);
+//        _clazzAndMsgCodeMap.put(GameMsgProtocol.WhoElseIsHereResult.class,GameMsgProtocol.MsgCode.WHO_ELSE_IS_HERE_CMD_VALUE);
+//        _clazzAndMsgCodeMap.put(GameMsgProtocol.UserMoveToResult.class,GameMsgProtocol.MsgCode.USER_MOVE_TO_CMD_VALUE);
+//        _clazzAndMsgCodeMap.put(GameMsgProtocol.UserQuitResult.class,GameMsgProtocol.MsgCode.USER_QUIT_RESULT_VALUE);
     }
     /**
      * 根据消息编号,获取消息构建器
@@ -69,7 +133,7 @@ public final class GameMsgRecognizer {
             return -1;
         }
 
-        Integer msgCode=_clazzAndMsgCodeMap.get(msgClazz);
+        Integer msgCode=_msgClazzAndMsgCodeMap.get(msgClazz);
         if(null==msgCode){
             return -1;
         }else{
